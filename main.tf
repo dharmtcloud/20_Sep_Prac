3fa05dae6667310d8989aafddc187cfb2b814fad
dharmtcloud <tvs.dharmendra@gmail.com>
2026-09-20 17:54:18 +0530

    featurekacode

--- main.tf
+++ main.tf
@@ -8,3 +8,9 @@ resource"azurerm_resource_group" "dharm1" {
   name     = "dharmendra_resource_block1"
   location = "West Europe"
 } 
+
+resource"azurerm_resource_group""dharm2" {
+  name     = "dharmendra_resource_block2"
+  location = "West Europe"
+}
+
