fec54e7ab697600f2a608a9234f6c3d6be559353
dharmtcloud <tvs.dharmendra@gmail.com>
2026-09-20 17:24:47 +0530

    Add Axion Terraform infrastructure-2

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource"azurerm_resource_group" "dharm" {
   name     = "dharmendra_resource_block"
   location = "West Europe"
 } 
+
+
+resource"azurerm_resource_group" "dharm1" {
+  name     = "dharmendra_resource_block1"
+  location = "West Europe"
+} 
